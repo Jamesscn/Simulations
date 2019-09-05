abfe8ee9e548eb7e4712486ca9700bb3c2db47f4
James Scoon <jamesscn@gmail.com>
2019-09-05 05:08:44 -0500

    Tensorflow digit recognition
    Created a simulation that allows users to play with a pre-trained CNN that recognizes digits

--- sims/mnist.js
+++ sims/mnist.js
@@ -1,4 +1,6 @@
-const model = tf.loadLayersModel('https://jamesscn.github.io/Simulations/sims/model.json');
+const model = tf.loadLayersModel('https://jamesscn.github.io/Simulations/sims/numbers.json').then(model => {
+    loadedModel = model
+});
 
 var canvasElement = document.getElementById("simulator")
 var canvas = canvasElement.getContext("2d")
@@ -13,11 +15,13 @@ var prevMouseY = 0
 var mouseDown = false
 var pencilX = [0, 0.3, 1, 0.7, 0]
 var pencilY = [0, 0, 0.7, 1, 0.3]
-var brushSize = 4
-var prediction = []
+var brushSize = 10
+var loadedModel = null
+var started = false
 
 document.body.onmousedown = function () {
     mouseDown = true
+    started = true
 }
 
 document.body.onmouseup = function () {
@@ -40,21 +44,37 @@ window.onload = function () {
     imageData = new ImageData(new Uint8ClampedArray(pixels), width, height)
     canvas.putImageData(imageData, 0, 0)
     setInterval(function () {
-        pixels = []
-        for(var i = 0; i < 28; i++) {
-            for(var j = 0; j < 28; j++) {
-                sum = 0
-                for(var x = 0; x < width / 28; x++) {
-                    for(var y = 0; y < height / 28; y++) {
-                        sum += imageData.data[((i * width / 28 + x) + (j * height / 28 + y) * width) * 4]
+        if(loadedModel != null) {
+            pixels = new Array(784)
+            for(var i = 0; i < 28; i++) {
+                for(var j = 0; j < 28; j++) {
+                    val = 0
+                    for(var x = 0; x < width / 28; x++) {
+                        for(var y = 0; y < height / 28; y++) {
+                            if(imageData.data[((i * width / 28 + x) + (j * height / 28 + y) * width) * 4] > 0) {
+                                val = 1
+                                x = width / 28
+                                y = height / 28
+                            }
+                        }
                     }
+                    pixels[j * 28 + i] = val
                 }
-                total = width * height / 784
-                pixels.push(sum / total)
+            }
+            pixelTensor = tf.tensor4d(pixels, [1, 28, 28, 1])
+            answers = loadedModel.predict(pixelTensor).arraySync()[0]
+            var maxVal = 0
+            var maxNum = 0
+            for(var i = 0; i < 10; i++) {
+                if(answers[i] > maxVal) {
+                    maxVal = answers[i]
+                    maxNum = i
+                }
+            }
+            if(started) {
+                nVal.textContent = maxNum
             }
         }
-        pixelTensor = tf.tensor4d(pixels, [1, 28, 28, 1])
-        prediction = model.predict(pixelTensor)
     }, 1000)
     draw()
 }
@@ -86,7 +106,7 @@ function draw() {
 }
 
 function drawPencil(x, y, size) {
-    size *= 30
+    size *= 15
     canvas.fillStyle = "rgb(255, 170, 109)"
     canvas.beginPath()
     canvas.moveTo(x + size * pencilX[0], y - size * pencilY[0])
@@ -102,7 +122,7 @@ function drawPencil(x, y, size) {
     canvas.lineTo(x + size * pencilX[4], y - size * pencilY[4])
     canvas.fill()
 
-    size *= 0.4
+    size *= 0.2
     canvas.fillStyle = "rgb(255, 255, 255)"
     canvas.beginPath()
     canvas.moveTo(x + size * pencilX[0], y - size * pencilY[0])
