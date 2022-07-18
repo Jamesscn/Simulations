8bb13ae0cfcf75cde0b8e399b2e44257f3432316
James Scoon <jamesscn@gmail.com>
2022-07-18 18:55:09 -0500

    Change cover page design

--- generateHtml.py
+++ generateHtml.py
@@ -14,25 +14,54 @@ template = """\
 		<link href="website/index.css" rel="stylesheet">
 		<title>James' Simulations</title>
 	</head>
-	<body class="bg-success text-light">
-		<div class="text-center mx-5 my-5">
-			<h1><strong>James' Simulations</strong></h1>
+	<body class="">
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a href="https://jamesscn.github.io/" class="navbar-brand">James Scoon</a>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a class="nav-link" href="https://jamesscn.github.io/about.html">About Me</a></li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Games
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="https://jamesscn.github.io/pacman/">Pacman</a></li>
+                            <li><a class="dropdown-item" href="https://jamesscn.github.io/wordscramble/">Wordscramble</a></li>
+                            <li><a class="dropdown-item" href="https://jamesscn.github.io/chipeight/">Chip-8 Emulator</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="https://github.com/Jamesscn/Tetris">Tetris</a></li>
+                            <li><a class="dropdown-item" href="https://github.com/Jamesscn/Snake">Snake</a></li>
+                            <li><a class="dropdown-item" href="https://github.com/Jamesscn/minefield">Minesweeper</a></li>
+                        </ul>
+                    </li>       
+                    <li class="nav-item"><a class="nav-link" href="https://jamesscn.github.io/Simulations/">Simulations</a></li>
+                    <li class="nav-item"><a class="nav-link" href="https://jamesscn.github.io/Knowledge/">Tutorials</a></li>
+                </ul>
+            </div>
+        </nav>
+
+		<div class="container px-2 py-2">
+		<div class="text-center mx-5 my-5 text-center">
+			<h1 class="display-4"><strong>Simulations</strong></h1>
 			<p>I find some things best explained with simulations. Some of these simulations are intented to help explain a concept, and others are just me messing around with different tools. Nevertheless, I thought it would be nice to share this with the world, so I hope you enjoy!</p>
 		</div>
+		<hr class="my-5">
 
-		<div class="bg-dark text-light">
-		<div class="container px-2 py-5 text-center">
-		<div class="row gx-5 gy-5 justify-content-center">
+		<div class="row gx-5 gy-5 justify-content-center text-center">
 		{0}
 		</div>
+		<hr class="mt-5">
+        <footer class="container">
+            <p>Made by James Scoon</p>
+        </footer>
 		</div>
-		</div>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 	</body>
 </html>
 """
 
 card = """\
-<div class="card col-3 px-0 mx-5 text-dark">
+<div class="card rounded-4 col-3 px-0 mx-5 text-dark">
 <h5 class="card-header"><strong>{0}</strong></h5>
 <i class="fa-solid fa-{3} mt-3" style="font-size: 5rem; color: {4};"></i>
 <div class="card-body">
